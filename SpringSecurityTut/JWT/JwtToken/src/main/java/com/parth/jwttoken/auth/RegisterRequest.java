59621a185c9e520ceaf978432ba09617b1336e7f
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-06-17 16:03:49 +0530

    for user registration

---
+++ SpringSecurityTut/JWT/JwtToken/src/main/java/com/parth/jwttoken/auth/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.parth.jwttoken.auth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}

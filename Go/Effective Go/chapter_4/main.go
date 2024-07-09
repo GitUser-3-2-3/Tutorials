301f4bc8b0fae747d2a6f0e243d1c26fec1b7f0e
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-07-09 15:40:34 +0530

    commit 1

--- Go/Effective Go/chapter_4/main.go
+++ Go/Effective Go/chapter_4/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
+type MyFuncOpts struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
+func main() {
+	println()
+
+	result, remainder, err := divAndRemainder(23, 3)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(result, remainder)
+}
+
+func addToBase(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, val := range vals {
+		out = append(out, base+val)
+	}
+	return out
+}
+
+func divAndRemainder(num, denom int) (result int, remainder int, err error) {
+	if denom == 0 {
+		err = errors.New("cannot divide by zero")
+		return 0, 0, err
+	}
+	result, remainder = num/denom, num%denom
+	return result, remainder, err
 }

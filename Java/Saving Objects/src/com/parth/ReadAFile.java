bb2782f717565618c98e711c40aac23491151fa5
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-07-29 21:57:49 +0530

    buffered reader

---
+++ Java/Saving Objects/src/com/parth/ReadAFile.java
@@ -0,0 +1,24 @@
+package com.parth;
+
+import java.io.*;
+
+public class ReadAFile {
+    public static void main(String[] args) {
+        System.out.println();
+
+        try {
+            File myFile = new File("MyText.txt");
+            FileReader fileReader = new FileReader(myFile);
+
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+}

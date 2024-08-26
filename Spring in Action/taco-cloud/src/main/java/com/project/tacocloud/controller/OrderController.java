ce9477ecaaec240a5b28a0650ec974b6de1e0f7c
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-26 11:24:08 +0530

    added a post method to submit and log the creation

--- Spring in Action/taco-cloud/src/main/java/com/project/tacocloud/controller/OrderController.java
+++ Spring in Action/taco-cloud/src/main/java/com/project/tacocloud/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.project.tacocloud.controller;
 
+import com.project.tacocloud.domain.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -15,5 +18,12 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus) {
+        log.info("Order Submitted: {}", tacoOrder);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }

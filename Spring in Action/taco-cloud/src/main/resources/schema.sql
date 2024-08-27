b08947c9900c17196a018fe0e5d2d62d98b0c25a
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-27 16:40:14 +0530

    changes made to work with postgres

--- Spring in Action/taco-cloud/src/main/resources/schema.sql
+++ Spring in Action/taco-cloud/src/main/resources/schema.sql
@@ -1,44 +1,38 @@
 create table if not exists Taco_Order
 (
-    id identity,
-    delivery_name varchar(50) not null,
+    id              serial primary key,
+    delivery_name   varchar(50) not null,
     delivery_street varchar(50) not null,
-    delivery_city varchar(50) not null,
-    delivery_state varchar(20) not null,
-    delivery_zip varchar(10) not null,
-    cc_number varchar(16) not null,
-    cc_expiration varchar(5) not null,
-    cc_cvv varchar(3) not null,
-    placed_at timestamp not null,
-    primary key (id)
+    delivery_city   varchar(50) not null,
+    delivery_state  varchar(20) not null,
+    delivery_zip    varchar(10) not null,
+    cc_number       varchar(16) not null,
+    cc_expiration   varchar(5)  not null,
+    cc_cvv          varchar(3)  not null,
+    placed_at       timestamp   not null
+);
+
+create table if not exists Ingredient
+(
+    id   varchar(4) primary key,
+    name varchar(25) not null,
+    type varchar(10) not null
 );
 
 create table if not exists Taco
 (
-    id identity,
-    name varchar(50) not null,
-    taco_order bigint not null,
-    taco_order_key bigint not null,
-    created_at timestamp not null,
-    primary key (id)
+    id             serial primary key,
+    name           varchar(50) not null,
+    taco_order     bigint      not null,
+    taco_order_key bigint      not null,
+    created_at     timestamp   not null,
+    foreign key (taco_order) references Taco_Order (id)
 );
 
 create table if not exists Ingredient_Ref
 (
     ingredient varchar(4) not null,
-    taco bigint not null,
-    taco_key bigint not null
+    taco       bigint     not null,
+    taco_key   bigint     not null,
+    foreign key (ingredient) references Ingredient (id)
 );
-
-create table if not exists Ingredient
-(
-    id varchar(4) not null,
-    name varchar(25) not null,
-    type varchar(10) not null,
-    primary key (id)
-);
-
-alter table Taco
-    add foreign key (taco_order) references Taco_Order(id);
-alter table Ingredient_Ref
-    add foreign key (ingredient) references Ingredient(id);

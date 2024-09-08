076d2db7ddd6898f6050ca1a345e65cb0be63c45
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-08 18:23:18 +0530

    adjust for security

---
+++ Spring in Action/taco-cloud/src/main/java/com/project/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.project.tacocloud.security;
+
+import com.project.tacocloud.data.UserRepository;
+import com.project.tacocloud.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findUserByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(req ->
+            req.requestMatchers("/design", "/orders").hasRole("USER")
+                .anyRequest().permitAll()
+        ).formLogin(form ->
+            form.loginPage("/login")
+                .defaultSuccessUrl("/design", true));
+
+        return http.build();
+    }
+}
+
+
+
+
+
+
+
+
+
+

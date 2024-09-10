6ddd3a6c13ba82497cb6889cc8835a787f8ceeec
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-10 20:49:59 +0530

    project initialized with default values and function skeletons

--- Golang/Database Project/main.go
+++ Golang/Database Project/main.go
@@ -1,25 +1,192 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+const Version = "1.0.1"
 
-func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
-	}
+type Address struct {
+	City    string
+	State   string
+	Country string
+	PinCode json.Number
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
+
+func main() {
+	directory := "./"
+
+	database, err := New(directory, nil)
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+	employees := []User{
+		{
+			Name:    "John",
+			Age:     "23",
+			Contact: "9335582338",
+			Company: "Amazon",
+			Address: Address{
+				City:    "Bangalore",
+				State:   "Karnataka",
+				Country: "India",
+				PinCode: "273008",
+			},
+		},
+		{
+			Name:    "Alice",
+			Age:     "30",
+			Contact: "9123456789",
+			Company: "Google",
+			Address: Address{
+				City:    "Mountain View",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94043",
+			},
+		},
+		{
+			Name:    "Bob",
+			Age:     "28",
+			Contact: "9876543210",
+			Company: "Microsoft",
+			Address: Address{
+				City:    "Redmond",
+				State:   "Washington",
+				Country: "USA",
+				PinCode: "98052",
+			},
+		},
+		{
+			Name:    "Charlie",
+			Age:     "35",
+			Contact: "9988776655",
+			Company: "Facebook",
+			Address: Address{
+				City:    "Menlo Park",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94025",
+			},
+		},
+		{
+			Name:    "David",
+			Age:     "40",
+			Contact: "9112233445",
+			Company: "Apple",
+			Address: Address{
+				City:    "Cupertino",
+				State:   "California",
+				Country: "USA",
+				PinCode: "95014",
+			},
+		},
+		{
+			Name:    "Eve",
+			Age:     "27",
+			Contact: "9223344556",
+			Company: "Netflix",
+			Address: Address{
+				City:    "Los Gatos",
+				State:   "California",
+				Country: "USA",
+				PinCode: "95032",
+			},
+		},
+		{
+			Name:    "Frank",
+			Age:     "32",
+			Contact: "9334455667",
+			Company: "Twitter",
+			Address: Address{
+				City:    "San Francisco",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94103",
+			},
+		},
+		{
+			Name:    "Grace",
+			Age:     "29",
+			Contact: "9445566778",
+			Company: "LinkedIn",
+			Address: Address{
+				City:    "Sunnyvale",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94085",
+			},
+		},
+		{
+			Name:    "Hank",
+			Age:     "26",
+			Contact: "9556677889",
+			Company: "Tesla",
+			Address: Address{
+				City:    "Palo Alto",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94304",
+			},
+		},
+		{
+			Name:    "Ivy",
+			Age:     "31",
+			Contact: "9667788990",
+			Company: "Uber",
+			Address: Address{
+				City:    "San Francisco",
+				State:   "California",
+				Country: "USA",
+				PinCode: "94105",
+			},
+		},
+	}
+
+	for _, value := range employees {
+		database.Write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
+
+	records, err := database.ReadAll("users")
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+	fmt.Println(records)
+
+	var allUsers []User
+
+	for _, record := range records {
+		employeeFound := User{}
+		if err := json.Unmarshal([]byte(record), &employeeFound); err != nil {
+			fmt.Println("Error ", err)
+		}
+		allUsers = append(allUsers, employeeFound)
+	}
+	fmt.Println(allUsers)
+
+	/*
+		if err := database.Delete("user", "john"); err != nil {
+			fmt.Println("Error ", err)
+		}
+
+		if err := database.Delete("user", ""); err != nil {
+			fmt.Println("Error ", err)
+		}
+	*/
+}

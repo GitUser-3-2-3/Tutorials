c6beb8999ed39dcfa69ab1033d904db0f5fb440f
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-12 17:50:53 +0530

    added methods for rendering walls

--- Golang/Not a Game/main.go
+++ Golang/Not a Game/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 const (
@@ -11,61 +12,91 @@ const (
 	PLAYER  = 69
 )
 
+type Level struct {
+	width, height int
+	data          [][]byte
+}
+
+func newLevel(width, height int) *Level {
+	data := make([][]byte, height)
+
+	for h := 0; h < height; h++ {
+		for w := 0; w < width; w++ {
+			data[h] = make([]byte, width)
+		}
+	}
+	return &Level{
+		width:  width,
+		height: height,
+		data:   data,
+	}
+}
+
+// todo -> potential bug! check for merger with newLevel before being used in renderLevel
+func setWalls(level *Level) {
+	for h := 0; h < level.height; h++ {
+		for w := 0; w < level.width; w++ {
+			if h == 0 || h == level.height-1 || w == 0 || w == level.width-1 {
+				level.data[h][w] = WALL
+			}
+		}
+	}
+}
+
 type Game struct {
+	isRunning bool
+	level     *Level
+	drawBuf   *bytes.Buffer
+}
+
+func newGame(width, height int) *Game {
+	lvl := newLevel(width, height)
+	return &Game{
+		level:   lvl,
+		drawBuf: new(bytes.Buffer),
+	}
+}
+
+func (game *Game) start() {
+	game.isRunning = true
+	game.loop()
+}
+
+func (game *Game) loop() {
+	for game.isRunning {
+		game.update()
+		game.render()
+	}
 }
 
 func (game *Game) update() {
 	// update
 }
 
+func (game *Game) renderLevel() {
+	setWalls(game.level)
+
+	for h := 0; h < game.level.height; h++ {
+		for w := 0; w < game.level.width; w++ {
+
+			if game.level.data[h][w] == NOTHING {
+				game.drawBuf.WriteString(" ")
+			} else if game.level.data[h][w] == WALL {
+				game.drawBuf.WriteString("▣")
+			}
+		}
+		game.drawBuf.WriteString("\n")
+	}
+}
+
 func (game *Game) render() {
-	// render
-}
-
-func initializeLevel(width, height int) [][]byte {
-	level := make([][]byte, height)
-
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			level[h] = make([]byte, width)
-		}
+	game.renderLevel()
+	if _, err := fmt.Fprint(os.Stdout, game.drawBuf.String()); err != nil {
+		fmt.Printf("ERROR in 'render': %v", err)
 	}
-	return level
-}
-
-func setWalls(level [][]byte, width, height int) {
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			if h == 0 || h == height-1 || w == 0 || w == width-1 {
-				level[h][w] = WALL
-			}
-		}
-	}
-}
-
-func renderWalls(level [][]byte, width, height int) string {
-	buf := new(bytes.Buffer)
-
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			if level[h][w] == NOTHING {
-				buf.WriteString(" ")
-			}
-			if level[h][w] == WALL {
-				buf.WriteString("H")
-			}
-		}
-		buf.WriteString("\n")
-	}
-	return buf.String()
 }
 
 func main() {
-	fmt.Println()
 	width := 80
 	height := 15
-
-	level := initializeLevel(width, height)
-	setWalls(level, width, height)
-	fmt.Println(renderWalls(level, width, height))
 }

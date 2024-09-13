82e00e9f749089bf0fc216691bacfffa023c7439
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-13 09:37:34 +0530

    set player limit

--- Golang/Not a Game/game/player.go
+++ Golang/Not a Game/game/player.go
@@ -1,13 +1,30 @@
 package main
 
 type Player struct {
-	pos   Position
-	level *Level
+	pos     Position
+	level   *Level
+	reverse bool
 }
 
 func (player *Player) update() {
-	player.pos.x += 1
-	if player.pos.x == player.level.width-1 {
+	if player.reverse {
 		player.pos.x -= 1
+
+		if player.pos.x == player.level.width-1 {
+			player.pos.x -= 1
+			player.reverse = false
+		}
+		if player.pos.x == 0 {
+			player.reverse = false
+		}
+	}
+
+	if !player.reverse {
+		player.pos.x += 1
+
+		if player.pos.x == player.level.width-1 {
+			player.pos.x -= 1
+			player.reverse = true
+		}
 	}
 }

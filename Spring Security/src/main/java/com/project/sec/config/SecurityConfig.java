a9784e89237926031fed323be0e0ee693c7c4424
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-10-03 11:45:42 +0530

    custom security filter chain

---
+++ Spring Security/src/main/java/com/project/sec/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.project.sec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        security.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+
+        return security.build();
+    }
+}

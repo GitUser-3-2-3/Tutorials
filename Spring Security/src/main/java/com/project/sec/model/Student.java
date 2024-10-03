877319abd0f20e216adc47947c50825dcf656de9
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-10-03 11:45:24 +0530

    clean up

--- Spring Security/src/main/java/com/project/sec/model/Student.java
+++ Spring Security/src/main/java/com/project/sec/model/Student.java
@@ -1,15 +1,28 @@
 package com.project.sec.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student {
 
     private Integer id;
     private String name;
     private Integer marks;
+
+    public Student(Integer id, String name, Integer marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", marks=" + marks +
+            '}';
+    }
 }

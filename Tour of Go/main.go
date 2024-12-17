46939080934f27d3fb56206098952a322e9ec7f8
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-12-17 14:25:39 +0530

    Named return values

--- Tour of Go/main.go
+++ Tour of Go/main.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Print(add(4, 1))
+
+	a, b := swap("Srivastav", "Parth ")
+	fmt.Print("\n", a, b)
+
+	fmt.Println()
+	fmt.Print(split(20))
+}
